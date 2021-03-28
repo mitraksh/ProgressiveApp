fb24984a66c0251188b8ad9b5c57097fd94caa8e
mitraksh <rathodmitraksh@gmail.com>
2021-03-28 15:46:08 +0530

    fetch

--- service-worker.js
+++ service-worker.js
@@ -26,3 +26,18 @@ self.addEventListener('install', (e) => {
         })
     );
 });
+
+self.addEventListener('fetch', function (e){
+    e.respondWith(
+        caches.match(e.request.then(function (r){
+            return r || fetch(e.request).then(function (response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request, response.clone());
+                    return response;
+                })
+            })
+            console.log('Service worker...Fetching resources' +e.request.url);
+            return r
+        } ))
+    )
+})
